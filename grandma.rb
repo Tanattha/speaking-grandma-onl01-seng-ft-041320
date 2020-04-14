0ddf6fb38fe586937239f4944daf7ea15f85306e
Tanattha Thuenim <tanattha.thuenim@gmail.com>
2020-04-14 15:38:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@
 def speak_to_grandma(gretting)
 
  
-  if gretting.upcase != true 
+  if gretting != gretting.upcase 
      return "HUH?! SPEAK UP, SONNY!"
   elsif gretting == "I LOVE YOU GRANDMA!"
      return "I LOVE YOU TOO PUMPKIN!"
